e1035126b6560b396d48667171753ddf29cb6df4
Daniel Griffiths <logiconictvtube@gmail.com>
2017-01-27 09:22:10 -0500

    alternate answers

--- code_wars.rb
+++ code_wars.rb
@@ -105,3 +105,11 @@ def even_or_odd(number)
     return "Odd"
   end
 end
+
+def even_or_odd(number)
+  number.even? ? "Even" : "Odd"
+end
+
+def even_or_odd(number)
+  (number % 2 == 0) ? "Even" : "Odd"
+end
