18f813029c3b9f479ab40134786af32ec0d9ea61
Daniel Griffiths <logiconictvtube@gmail.com>
2017-02-10 09:46:14 -0500

    Find the Barycenter of a triangle

--- ruby_code_wars.rb
+++ ruby_code_wars.rb
@@ -488,3 +488,21 @@ def averages(arr)
     return result
   end
 end
+
+#51 Find the Barycenter of a triangle
+def bar_triang(p1,p2,p3)
+  barycenter = Array.new
+  topx = p1[0] + p2[0] + p3[0]
+  topy = p1[1] + p2[1] + p3[1]
+  x = topx.to_f / 3
+  y = topy.to_f / 3
+  barycenter << x.round(4)
+  barycenter << y.round(4)
+  return barycenter
+end
+
+def bar_triang(p1,p2,p3)
+  x = (p1[0] + p2[0] + p3[0]) / 3.0
+  y = (p1[1] + p2[1] + p3[1]) / 3.0
+  [x.round(4),y.round(4)]
+end
