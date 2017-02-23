cc3ece262dbc127d7af5126e03aa499c63d8bd75
Daniel Griffiths <logiconictvtube@gmail.com>
2017-02-23 08:57:39 -0500

    74, README, description

--- README.md
+++ README.md
@@ -245,3 +245,12 @@ Continue this pattern until you have done every single position, and then you wi
 
 
 73 We need a function that can transform a string into a number. (Thought I'd knock this out for some easy points)
+
+
+
+74 You start at the first number.
+ If this number x is smaller than 3, take this number x direct
+  for the new array and continue with the next number.
+ If this number x is greater than 2, take the next x numbers (inclusive this number) as a
+  sub-array in the new array. Continue with the next number AFTER this taken numbers.
+ If there are too few numbers to take by number, take the last available numbers.
--- ruby_code_wars.rb
+++ ruby_code_wars.rb
@@ -788,3 +788,18 @@ end
 def string_to_number(s)
   s.to_i
 end
+
+# 74  You start at the first number.
+#  If this number x is smaller than 3, take this number x direct
+#   for the new array and continue with the next number.
+#  If this number x is greater than 2, take the next x numbers (inclusive this number) as a
+#   sub-array in the new array. Continue with the next number AFTER this taken numbers.
+#  If there are too few numbers to take by number, take the last available numbers.
+def unflatten(flat_array)
+  new_array = []
+  until flat_array.empty?
+    num = flat_array.shift
+    num < 3 ? new_array << num : new_array << [num].push(flat_array.shift(num-1)).flatten
+  end
+  new_array
+end
